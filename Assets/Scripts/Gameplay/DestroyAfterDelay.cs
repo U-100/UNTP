34e10a0325dbc63acd7423870bbad2013956d5dd
Yuri Sedyakin <y.sedyakin@corp.mail.ru>
2023-12-05 19:49:23 +0300

    cleanup explosion leftovers

---
+++ Assets/Scripts/Gameplay/DestroyAfterDelay.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+
+namespace UNTP
+{
+    public class DestroyAfterDelay : MonoBehaviour
+    {
+        [SerializeField] private float destructionDelay = 1f;
+
+        private void Awake() => Destroy(this.gameObject, this.destructionDelay);
+    }
+}
