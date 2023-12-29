80be6e72dfa6693fbd81bed84b9d5e0f188591e3
Yuri Sedyakin <y.sedyakin@corp.mail.ru>
2023-12-29 11:11:34 +0300

    better walking player character

--- Assets/Scripts/Gameplay/NetworkPlayerCharacter.cs
+++ Assets/Scripts/Gameplay/NetworkPlayerCharacter.cs
@@ -4,69 +4,90 @@ using Unity.Netcode;
 using UnityEngine;
 using UnityEngine.VFX;
 using UnityEngine.VFX.Utility;
-
 using static Unity.Mathematics.math;
 
 namespace UNTP
 {
-	// simple player character object whose position/rotation are synced over the network
-	public class NetworkPlayerCharacter : NetworkBehaviour, IPlayerCharacter
-	{
-		[SerializeField] private CinemachineCamera _playerCamera;
-		[SerializeField] private VisualEffect _shotEffect;
-		[SerializeField] private Transform _aimPositionTransform;
+    // simple player character object whose position/rotation are synced over the network
+    public class NetworkPlayerCharacter : NetworkBehaviour, IPlayerCharacter
+    {
+        [SerializeField] private CinemachineCamera _playerCamera;
+        [SerializeField] private VisualEffect _shotEffect;
+        [SerializeField] private Transform _aimPositionTransform;
+        [SerializeField] private Animator _animator;
 
-		private VFXEventAttribute _vfxEventAttribute;
-		private readonly ExposedProperty _vfxPropertySource = "source";
-		private readonly ExposedProperty _vfxPropertyTarget = "target";
-		private readonly ExposedProperty _vfxPropertyHitNormal = "hitNormal";
+        private Transform _cachedTransform;
+        private Vector3 _savedPosition;
 
-		public float3 position
-		{
-			get => this.transform.position;
-			set => this.transform.position = value;
-		}
+        private int? _speedAnimatorParamHash;
 
-		public quaternion rotation
-		{
-			get => this.transform.rotation;
-			set => this.transform.rotation = value;
-		}
+        private VFXEventAttribute _vfxEventAttribute;
 
-		public float3 forward
-		{
-			get => this.transform.forward;
-			set => this.transform.forward = value;
-		}
-		
-		public float timeSinceLastShot { get; set; }
+        private readonly ExposedProperty _vfxPropertySource = "source";
+        private readonly ExposedProperty _vfxPropertyTarget = "target";
+        private readonly ExposedProperty _vfxPropertyHitNormal = "hitNormal";
 
-		public void Shoot(float3 from, float3 target, float3 hitNormal) => ShootServerRpc(from, target, hitNormal);
+        public float3 position
+        {
+            get => this.transform.position;
+            set => this.transform.position = value;
+        }
 
-		[ServerRpc]
-		private void ShootServerRpc(float3 from, float3 target, float3 hitNormal) => ShootClientRpc(from, target, hitNormal);
+        public quaternion rotation
+        {
+            get => this.transform.rotation;
+            set => this.transform.rotation = value;
+        }
 
-		[ClientRpc]
-		private void ShootClientRpc(float3 from, float3 target, float3 hitNormal)
-		{
-			this._aimPositionTransform.position = target;
-			
-			this._vfxEventAttribute.SetVector3(this._vfxPropertySource, from);
-			this._vfxEventAttribute.SetVector3(this._vfxPropertyTarget, target);
-			this._vfxEventAttribute.SetVector3(this._vfxPropertyHitNormal, hitNormal);
-			this._shotEffect.Play(this._vfxEventAttribute);
-		}
-		
-		public CinemachineCamera playerCamera => this._playerCamera;
-		
-		public override void OnNetworkSpawn()
-		{
-			this._playerCamera.gameObject.SetActive(this.IsClient && this.IsOwner);
-		}
+        public float3 forward
+        {
+            get => this.transform.forward;
+            set => this.transform.forward = value;
+        }
 
-		void Start()
-		{
-			this._vfxEventAttribute = this._shotEffect.CreateVFXEventAttribute();
-		}
-	}
+        public float timeSinceLastShot { get; set; }
+
+        public void Shoot(float3 from, float3 target, float3 hitNormal) => ShootServerRpc(from, target, hitNormal);
+
+        [ServerRpc]
+        private void ShootServerRpc(float3 from, float3 target, float3 hitNormal) => ShootClientRpc(from, target, hitNormal);
+
+        [ClientRpc]
+        private void ShootClientRpc(float3 from, float3 target, float3 hitNormal)
+        {
+            this._aimPositionTransform.position = target;
+
+            this._vfxEventAttribute.SetVector3(this._vfxPropertySource, from);
+            this._vfxEventAttribute.SetVector3(this._vfxPropertyTarget, target);
+            this._vfxEventAttribute.SetVector3(this._vfxPropertyHitNormal, hitNormal);
+            this._shotEffect.Play(this._vfxEventAttribute);
+        }
+
+        public CinemachineCamera playerCamera => this._playerCamera;
+
+        public override void OnNetworkSpawn()
+        {
+            this._playerCamera.gameObject.SetActive(this.IsClient && this.IsOwner);
+        }
+
+        void Start()
+        {
+            this._vfxEventAttribute = this._shotEffect.CreateVFXEventAttribute();
+
+            this._cachedTransform = this.transform;
+            this._savedPosition = this._cachedTransform.position;
+        }
+
+        void Update()
+        {
+            float3 positionDelta = this.transform.position - this._savedPosition;
+            this._savedPosition = this._cachedTransform.position;
+
+            if (Time.deltaTime > Mathf.Epsilon)
+            {
+                float speed = clamp(length(positionDelta) / Time.deltaTime, 0, 3);
+                this._animator.SetFloat(this._speedAnimatorParamHash ??= Animator.StringToHash("speed"), speed);
+            }
+        }
+    }
 }
